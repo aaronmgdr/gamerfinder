dc4a9383e47f9510d5014af90c32ef5088996e50
MakerSquare Student <students@themakersquare.com>
2013-10-19 13:20:19 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gamerfinder::Application.config.secret_key_base = '2380bea43f9bb70d1ddf305a0c2450ae307aa83805eb6a8fdc68867719deb803e6bc78e5372d9901724b14a294d2c63555f92d49d9ed39d22d4aeeebaf7524f4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gamerfinder::Application.config.session_store :cookie_store, key: '_gamerfinder_session'
