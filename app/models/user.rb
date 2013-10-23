67779548277f7d6899c9541968faf084157689b2
MakerSquare Student <students@themakersquare.com>
2013-10-23 17:42:48 -0500

    add associations between questionsary models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-
+  has_many :responses
+  has_many :answers, through: :responses
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
