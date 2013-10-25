6002746be8598f8e4979488029a2da589b632ba2
Nik Daftary <nikdaftary@gmail.com>
2013-10-25 13:55:59 -0500

    M:M matching system

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,48 +8,54 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  # def self.search(search)
-  #   if search
-  #     find(:all, :conditions => ['systems Like ?', "%#{search}%"])
-  #   else
-  #     find(:all)
-  #   end
-  # end
 
   def answers
+    # this picks the users's responses for all questions - returns an array of answers
     self.responses.pluck(:answer_id)
   end
 
-  def compare(user_id)
-    mine = self.answers
-    theirs= User.find(user_id).responses.pluck(:answer_id)
+# -------------------------------------------------------------
+  # This was originally built to help build compare_to_all
+  # def compare(user_id)
+  #   # my answers
+  #   mine = self.answers
 
-    answer_array_comparer(mine, theirs)
-  end
+  #   #next person's answers
+  #   # theirs= User.find(user_id).responses.pluck(:answer_id)
+  #   theirs= User.find(user_id).answers
+  #   answer_array_comparer(mine, theirs)
+  # end
+  # -------------------------------------------------------------
+
 
   def compare_to_all
     users = User.all
     @similarity = Hash.new {}
-    likeness = []
-
+    puts self.email
     users.each do |looped_user|
-     puts "#{looped_user.email} answers #{looped_user.answers}"
-     overlap = self.answers & looped_user.answers
-     puts "in Both #{overlap}"
+      puts "#{looped_user.email} answers #{looped_user.answers}"
+      answer_array_comparer(self.answers,looped_user.answers)
+      
 
-    likeness = overlap.length.to_f / looped_user.answers.length.to_f
 
-    puts "They are #{likeness*100}% similar"
+      # puts "in Both #{overlap}"
 
-      @similarity["#{looped_user.email}"] = likeness
+      # likeness = overlap.length.to_f / looped_user.answers.length.to_f
+
+      # puts "They are #{likeness*100}% similar"
+
+      @similarity["#{looped_user.email}"] = @likeness
     end
     return @similarity
   end
 
   private
 
+  # takes in both person's arrays.  
   def answer_array_comparer(my_array, their_array)
-    overlaping_answers = my_array & their_array
-    likeness = overlaping_answers.length.to_f / my_array.length.to_f
+    #overlapping_answers = only overlapped answers
+    overlapping_answers = my_array & their_array
+    #returns percent similarity between user a & user b
+    @likeness = (overlapping_answers.length.to_f / my_array.length.to_f)*100
   end
 end
