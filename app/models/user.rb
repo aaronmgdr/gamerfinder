19da8b822b780818ce9c7ed39cf659f3cb33da93
Fremen DeRuvo <fremenderuvo@gmail.com>
2013-10-26 16:26:01 -0500

    add TomDoc and comments to anwers and compare_to_all methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,48 +9,51 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  
 
-
+  # Internal: Gets calling users answers to all questions sorted lowest to highest
+  # Examples
+  #
+  #   answers
+  #   # => '[1,4,9,11]'
+  #
+  # Returns an Array of Integers
   def answers
-    # this picks the users's responses for all questions - returns an array of answers
-    self.responses.pluck(:answer_id)
+    self.responses.pluck(:answer_id).sort
   end
-
-
+  
+  # Internal: Generates a score the user calling method against all other users.
+  # Examples
+  #
+  #   compare_to_all
+  #   # =>   '+----+---------------+---------+------------------+-------------------------+-------------------------+
+            # | id | match_percent | user_id | compared_user_id | created_at              | updated_at              |
+            # +----+---------------+---------+------------------+-------------------------+-------------------------+
+            # | 1  | 0.5           | 1       | 2                | 2013-10-26 19:24:43 UTC | 2013-10-26 19:24:43 UTC |
+            # | 4  | 0.5           | 2       | 3                | 2013-10-26 19:42:55 UTC | 2013-10-26 19:42:55 UTC |
+            # | 5  | 0.25          | 2       | 4                | 2013-10-26 19:42:55 UTC | 2013-10-26 19:42:55 UTC |'
+  #
+  # Saves the results to the Comparisons Table 
   def compare_to_all
-    similarity = {}
-    User.all.each do |looped_user|
-      overlapping_answers = self.answers & looped_user.answers
+    User.all.each do |user_being_compared|
+      #
+      overlapping_answers = self.answers & user_being_compared.answers
       value = (overlapping_answers.length.to_f / answers.length.to_f)
 
-      unless self.id == looped_user.id
-        if Comparison.where(user_id:"#{self.id}", compared_user_id: "#{looped_user.id}").exists?
-          puts"update"          
-          User.find(self.id).comparisons.find_by_compared_user_id(looped_user.id).update(match_percent: "#{value}")
+      #Ensures user comparison with itself is not save to DB
+      unless self.id == user_being_compared.id
+        #Updates the record if it exists, otherwise creates a new association
+        if Comparison.where(user_id:"#{self.id}", compared_user_id: "#{user_being_compared.id}").exists?          
+          User.find(self.id).comparisons.find_by_compared_user_id(user_being_compared.id).update(match_percent: "#{value}")
         else
-         puts "create"
           Comparison.create(
             user_id:"#{self.id}", 
-            compared_user_id: "#{looped_user.id}",
+            compared_user_id: "#{user_being_compared.id}",
             match_percent: "#{value}"
           )
         end    
       end
+      return Comparison.all
     end
   end
 end
-
-
-
-# -------------------------------------------------------------
-  # This was originally built to help build compare_to_all
-  # def compare(user_id)
-  #   # my answers
-  #   mine = self.answers
-
-  #   #next person's answers
-  #   # theirs= User.find(user_id).responses.pluck(:answer_id)
-  #   theirs= User.find(user_id).answers
-  #   answer_array_comparer(mine, theirs)
-  # end
-  # -------------------------------------------------------------
