2737bd1fa3e3d8cb76aa895594bc2c9f6a4afcc6
MakerSquare Student <students@themakersquare.com>
2013-10-30 14:41:35 -0500

    add gamertag to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131029040810) do
+ActiveRecord::Schema.define(version: 20131030190751) do
 
   create_table "answers", force: true do |t|
     t.string   "choice"
@@ -82,6 +82,7 @@ ActiveRecord::Schema.define(version: 20131029040810) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+    t.string   "xbox_gamertag"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
