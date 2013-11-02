d030c0bed8438ca73e727bc592720cbbee632bf5
Nik Daftary <nikdaftary@gmail.com>
2013-11-02 14:59:44 -0500

    working user responses changes

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -22,13 +22,27 @@ class ResponsesController < ApplicationController
   end
 
   def bulk_update
-    
     # Response.create(user_id: params[:user_id], answer_id: 2)
-    Response.update(user_id: params[:user_id], answer_id: 2)
+    # Response.update(user_id: params[:user_id], answer_id: 2)
+
+      # Response.where('user_id=current_user').delete_all
+      
+      Response.delete_all("user_id = #{params[:user_id]}")
+      Response.create(user_id: params[:user_id], answer_id: params[:answer0])
+      Response.create(user_id: params[:user_id], answer_id: params[:answer1])
+      Response.create(user_id: params[:user_id], answer_id: params[:answer2])
+      Response.create(user_id: params[:user_id], answer_id: params[:answer3])
+      redirect_to users_url()
+
+
   end
-
-
 end
+
+    # I want to bulk_update all the users question responses
+    # On this page, go through each of the answer responses and 
+    # update the users.response record to include the user_id with the answer_id
+
+
 
 # [
 #     {user: user, answer: Answer.find(Random.rand(1..3))},
