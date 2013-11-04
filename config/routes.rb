832fa6f96835f5d52df463eda8f251988a2c8e04
Fremen DeRuvo <fremenderuvo@gmail.com>
2013-11-04 09:22:56 -0600

    fix routing error on sign up/in out: refactored xbox form to be remote true

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Gamerfinder::Application.routes.draw do
   root 'users#index'
   get "registrations_controller/after_sign_up_path_for"
-  devise_for :users, :controllers => { :registrations => "registrations" }#, :path => 'accounts'
+  devise_for :users, :controllers => { :registrations => "registrations" }, :path => 'accounts'
   delete '/conversations/:id', to: 'conversations#destroy', as: 'destroy'
   resources :xbox_gamer_info
   resources :users do
