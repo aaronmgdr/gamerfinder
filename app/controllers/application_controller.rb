81451e6bf0d605ccd9fd4bdc8231c0fe09cc2f78
Fremen DeRuvo <fremenderuvo@gmail.com>
2013-11-04 09:51:12 -0600

    add name field to signup form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  # def after_sign_up_path_for(user)
-  #   new_xbox_gamer_info_path
-  # end
+ 
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :name
+  end
+
 end
