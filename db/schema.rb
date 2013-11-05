7c1a73d8022033345f8b863ec2ffcf5476be53fa
Nik Daftary <nikdaftary@gmail.com>
2013-11-05 17:58:55 -0600

    skinny model curvy controller + storing hasGTA

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131101205353) do
+ActiveRecord::Schema.define(version: 20131105231743) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -139,6 +139,7 @@ ActiveRecord::Schema.define(version: 20131101205353) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "hasgtav"
   end
 
   add_foreign_key "notifications", "conversations", name: "notifications_on_conversation_id"
