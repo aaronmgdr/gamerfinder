58c804ea53d7d1971058d5f9f6b0890df049377c
Fremen DeRuvo <fremenderuvo@gmail.com>
2013-11-20 22:14:35 -0600

    fix merg error

--- app/models/xbox_gamer_info.rb
+++ app/models/xbox_gamer_info.rb
@@ -1,14 +1,10 @@
 class XboxGamerInfo < ActiveRecord::Base
   belongs_to :user
-<<<<<<< HEAD
-=======
 
   #to run this as a test, within irb, run the following >> XboxGamerInfo.new.get_xbox_info
   #you won't get a userid, because this would actually be filled in during the user's current session
 
 
-
->>>>>>> nik-comments
 end
 
 
